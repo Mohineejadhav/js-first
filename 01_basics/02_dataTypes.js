1acebb7f6c941f05d7536f3a7b7d77afb207cd88
Mohineejadhav <122805968+Mohineejadhav@users.noreply.github.com>
2024-03-14 11:16:00 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,2 +1,18 @@
 "use strict" //treat all js code as newer version
-// alert(3+3) we are using node not browser
+// alert(3+3) we are using node js not browser
+
+let name="Mohinee" //1.String
+let age=22         //2.Number
+let isLoggedIn=true//3.Boolean
+let num=1000000000000000000000000000//4.Binint
+let state;         //5.Undefined
+let temp=null      //6.null
+                   //7.Symbol
+                   //8.Object
+console.log(typeof "Mohinee")
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof temp)
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null)      //object
