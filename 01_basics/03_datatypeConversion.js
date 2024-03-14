87e970de9784926ae24b4532c6d356ccdce3de3f
Mohineejadhav <122805968+Mohineejadhav@users.noreply.github.com>
2024-03-14 12:34:46 +0000

    Conversion

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,30 @@
+/*--------Number Conversion-------*/
+let score="26"
+let convertedScore=Number(score)  //String is converted into number
+// console.log(typeof convertedScore);
+
+//"33"=33
+//"33qdr"=NaN Not A Number
+//true=1;false=0;
+//undefined=NaN
+//null=0
+
+
+/*--------Boolean Conversion-------*/
+let isLoggedIn=null 
+let convertBoolean=Boolean(isLoggedIn);
+// console.log(convertBoolean);
+
+//for 1 2 3 any number ==true for 0=false
+//for ""=false for nonempty string=true
+//undefined==false
+//null=false
+
+/*--------String Conversion-------*/
+let isstring=null
+let convertintoString=String(isstring);
+console.log(typeof convertintoString);
+
+//null,undefined,any number, boolean all can be converted into string 
+
+/* ***************************Operations************************** */
