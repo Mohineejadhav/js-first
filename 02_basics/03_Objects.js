ee2bdf9d78dea57a90a1f758bf912d5967610f26
Mohineejadhav <122805968+Mohineejadhav@users.noreply.github.com>
2024-03-20 07:44:44 +0000

    Objets1 done

---
+++ 02_basics/03_Objects.js
@@ -0,0 +1,32 @@
+//Singleton
+
+//Object Literals
+
+const sym=Symbol("mysym1")
+
+const JsUser={
+    name:"Mohinee",
+    "full name":"Mohinee Jadhav",
+    age:22,
+    [sym]:"mysym",  // Very imp symbols are written in this way in objets as key
+    email:"mohinee@google.com",
+    mobile:8010990850
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"])
+console.log(JsUser["full name"] )
+console.log(JsUser[sym])
+
+// Object.freeze(JsUser)
+// console.log(JsUser);
+
+JsUser["full name"]="Ram jadhav"
+JsUser[sym]="Mysym345"
+console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("My family happy family")
+}
+console.log(JsUser.greeting());
+
