4750943839a4fc17e80c2a2f660d8f826f23054e
Mohineejadhav <122805968+Mohineejadhav@users.noreply.github.com>
2024-03-21 07:19:36 +0000

    objects1 done

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -13,20 +13,26 @@ const JsUser={
     mobile:8010990850
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"])
-console.log(JsUser["full name"] )
-console.log(JsUser[sym])
+// console.log(JsUser.email);
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"] )
+// console.log(JsUser[sym])
 
 // Object.freeze(JsUser)
 // console.log(JsUser);
 
 JsUser["full name"]="Ram jadhav"
 JsUser[sym]="Mysym345"
-console.log(JsUser)
+// console.log(JsUser)
 
 JsUser.greeting = function(){
+    
     console.log("My family happy family")
 }
 console.log(JsUser.greeting());
+
+JsUser.greeting2= function(){
+    console.log(`I ${this.name} will do it anyway`);
+}
+console.log(JsUser.greeting2());
 
