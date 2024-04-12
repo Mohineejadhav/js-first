b91da413089834eac230ca0b93aa145924332546
Mohineejadhav <122805968+Mohineejadhav@users.noreply.github.com>
2024-04-12 06:30:56 +0000

    forOf done

---
+++ 05_Iterations/3_forOf.js
@@ -0,0 +1,49 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num); //1,2,3,4,5
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`) // prints each character of string  
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+for (const key of map) {
+     //console.log(key);
+}
+// [ 'IN', 'India' ]
+// [ 'USA', 'United States of America' ]
+// [ 'Fr', 'France' ]
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+// IN :- India
+// USA :- United States of America
+// Fr :- France
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value); //error => myObject is not iterable
+                                      // We can cot iterate object like this
+// }
