39166e569277acf4505586933d72c7d95ea597bc
Mohineejadhav <122805968+Mohineejadhav@users.noreply.github.com>
2024-04-12 05:20:40 +0000

    controlflow finished

---
+++ 04_controlflow/3_truthy.js
@@ -0,0 +1,53 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email"); //Got user email
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+//this is how we chack truthy values for Arrays and Objects
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+//The nullish coalescing operator (??) is a feature introduced in JavaScript 
+//to provide a convenient way to handle default values for null or undefined values.
+
+let val1;
+// val1 = 5 ?? 10  //5
+// val1 = null ?? 10 //10
+// val1 = undefined ?? 15 //15
+val1 = null ?? 10 ?? 20 //10
+
+console.log(val1);
+
+const someValue = null;
+const defaultValue = someValue ?? 'default';
+console.log(defaultValue); // Output: 'default'
+
+const falsyValue = '';
+const result = falsyValue ?? 'default';
+console.log(result); // Output: ''
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
